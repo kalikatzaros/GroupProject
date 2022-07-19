76392becf79e1235139845c4b871db85643b0691
Alexis Es Soureifi <alexisfmpro2007@yahoo.com>
2022-07-19 21:04:02 +0300

    Add Following Model

--- GroupProject/Models/ApplicationDbContext.cs
+++ GroupProject/Models/ApplicationDbContext.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 
 namespace GroupProject.Models
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Topic> Topics { get; set; }
+        public DbSet<TopicPost> TopicPosts { get; set; }
+
+        public DbSet<WallPost> WallPosts { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
