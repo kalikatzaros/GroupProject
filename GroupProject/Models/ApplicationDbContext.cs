83dd1f95504aecc1dd59ba1738f3cec4a204d703
Alexis Es Soureifi <alexisfmpro2007@yahoo.com>
2022-07-19 20:31:04 +0300

    Separating ApplicationUser and DbContext

---
+++ GroupProject/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext()
+        : base("DefaultConnection", throwIfV1Schema: false)
+    {
+    }
+
+    public static ApplicationDbContext Create()
+    {
+        return new ApplicationDbContext();
+    }
+}
