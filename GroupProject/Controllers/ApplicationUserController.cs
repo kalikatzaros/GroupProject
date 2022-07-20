6d03c0c7c167b9d4332db43548f7b60edd30afaa
Alexis Es Soureifi <alexisfmpro2007@yahoo.com>
2022-07-20 21:36:54 +0300

    fixed connectionString for Athens the weirdo

---
+++ GroupProject/Controllers/ApplicationUserController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GroupProject.Repositories;
+
+
+
+namespace GroupProject.Controllers
+{
+    public class ApplicationUserController : Controller
+    {
+        private readonly ApplicationUserRepository _repo;
+
+
+        public ApplicationUserController()
+        {
+            _repo = new ApplicationUserRepository();
+        }
+        
+        // GET: ApplicationUser
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult ActionName()
+        {
+            string userId = User.Identity.GetUserId();
+            return View();
+        }
+    }
+}
