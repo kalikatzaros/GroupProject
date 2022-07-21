38a57e0f93c2586d7f15cd798ba2810329ac5c2d
Alexis Es Soureifi <alexisfmpro2007@yahoo.com>
2022-07-22 01:03:09 +0300

    Added MessagesController and UserMessagesViewModel

---
+++ GroupProject/Controllers/MessagesController.cs
@@ -0,0 +1,37 @@
+﻿using GroupProject.Models;
+using GroupProject.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GroupProject.Controllers
+{
+    public class MessagesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MessagesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Messages
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CreateMessage()
+        {
+            var viewModel = new UserMessagesViewModel();
+            viewModel.Users = _context.Users;
+            return View(viewModel);
+        }
+
+
+
+
+    }
+}
---
+++ GroupProject/ViewModels/UserMessagesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using GroupProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GroupProject.ViewModels
+{
+    public class UserMessagesViewModel
+    {
+        public IEnumerable<ApplicationUser> Users { get; set; }
+        public Message Message { get; set; }
+    }
+}
