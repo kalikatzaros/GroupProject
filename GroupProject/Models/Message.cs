5186a6045ce3febaf4323b22c5680f80a2193ea4
Alexis Es Soureifi <alexisfmpro2007@yahoo.com>
2022-07-28 21:51:55 +0300

    Updated SentMessagesView

--- GroupProject/Models/Message.cs
+++ GroupProject/Models/Message.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Text;
 using System.Web;
 
 namespace GroupProject.Models
@@ -12,12 +13,35 @@ namespace GroupProject.Models
         public int Id { get; set; }
         public string SenderId { get; set; }
         public ApplicationUser Sender { get; set; }
-        public string ReceiverId { get; set; }  
+        public string ReceiverId { get; set; }
         public ApplicationUser Receiver { get; set; }
         [DataType(DataType.MultilineText)]
         public string Body { get; set; }
+        [NotMapped]
+        public string ShorterBody
+        {
+            get 
+            {
+               return GetShorterBody();
+
+            }
+        }
+
         [DataType(DataType.DateTime)]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Datetime { get; set; }
+
+
+
+        private string GetShorterBody()
+        {
+            if (Body.Length > 9)
+            {
+                var shortBody = Body.Substring(0, 10);
+                
+                return String.Concat(shortBody, "...");
+            }                        
+            return String.Concat(Body, "...");
+        }
     }
 }
