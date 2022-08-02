454ee36bcb0f0d376e92b1445a79c1ef3d0fd83f
Ioannis Rafail Syropoulos <sirrafail@gmail.com>
2022-08-03 01:44:08 +0300

    ProfileController

---
+++ GroupProject/Controllers/ProfileController.cs
@@ -0,0 +1,43 @@
+﻿using GroupProject.Models;
+using GroupProject.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GroupProject.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public ProfileController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Profile
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            var user = _context.Users.SingleOrDefault(u => u.Id == userId);
+            var viewModel = new ProfileViewModel()
+            {
+                User = user,
+                Email = user.Email,
+                ProfileImage =user.Thumbnail,
+                FirstName = user.Name,
+                LastName = user.LastName
+            };
+            return View(viewModel);
+        }
+
+        public ActionResult GetProfileImage()
+        {
+            var userId = User.Identity.GetUserId();
+            var user = _context.Users.SingleOrDefault(u => u.Id == userId);
+            return View(user.Thumbnail);
+        }
+    }
+}
---
+++ GroupProject/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,21 @@
+﻿using GroupProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GroupProject.ViewModels
+{
+    public class ProfileViewModel
+    {
+        public ApplicationUser User { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string ProfileImage { get; set; }
+        public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+
+    }
+}
