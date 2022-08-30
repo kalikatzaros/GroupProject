aea9ff92abce4be3a3d8e49fa49301ff2097cc9e
Athena Tounta <tounta.ath@gmail.com>
2022-08-30 21:43:00 +0300

    deleted minor stuff

---
+++ GroupProject/Controllers/API/FriendsController.cs
@@ -0,0 +1,17 @@
+﻿using GroupProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GroupProject.Controllers.API
+{
+    public class FriendsController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+    }
+   
+}
