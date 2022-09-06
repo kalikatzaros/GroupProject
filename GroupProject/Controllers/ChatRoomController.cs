1ef201803502eff18099540f1dcbfaf8ef6b4628
Alexis Es Soureifi <alexisfmpro2007@yahoo.com>
2022-09-06 23:56:03 +0300

    SignalR added

---
+++ GroupProject/Controllers/ChatRoomController.cs
@@ -0,0 +1,35 @@
+﻿using GroupProject.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GroupProject.Controllers
+{
+    public class ChatRoomController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ChatRoomController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: ChatRoom
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SignalRChat()
+        {
+
+            var userId = User.Identity.GetUserId();
+            var user = _context.Users.SingleOrDefault(u => u.Id == userId);
+            ViewBag.UserName = user.FullName;
+
+            return View();
+        }
+    }
+}
