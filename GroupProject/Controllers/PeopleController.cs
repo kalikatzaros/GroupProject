b9ea769afb744eb7ec4ccde011ff3d9055523ad9
Athena Tounta <tounta.ath@gmail.com>
2022-09-07 16:28:09 +0300

    signalr chat fix

--- GroupProject/Controllers/PeopleController.cs
+++ GroupProject/Controllers/PeopleController.cs
@@ -17,37 +17,7 @@ namespace GroupProject.Controllers
         {
             _context = new ApplicationDbContext();
         }
-        // GET: People
-
-        //public ActionResult Index(string search)
-        //{
-        //    var userId = User.Identity.GetUserId();
-
-        //    if (search == null)
-        //    {
-        //        var users = _context.Users
-        //            .Include(u=>u.Followees)
-        //            .Include(u =>u.Followers)
-        //            .Where(u => u.Id != userId).ToList();
-        //        ViewBag.userId = userId;
-        //        return View(users);
-        //    }
-            
-
-        //    var searched = _context.Users
-        //                 .Include(u => u.Followees)
-        //                 .Include(u => u.Followers)
-        //                 .Where(u => u.Id != userId &&( u.Name.ToLower() == search.ToLower() 
-        //                 || u.LastName.ToLower() == search.ToLower()
-        //                 || u.Email.ToLower()== search.ToLower()))
-        //                 .ToList();
-        //    ViewBag.userId = userId;
-        //    return View(searched);
-            
-        //}
-
-
-
+       
         public ActionResult Index(string search)
         {
             var userId = User.Identity.GetUserId();
