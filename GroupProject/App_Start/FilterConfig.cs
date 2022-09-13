99db0475f8b3ccee1b625598af668bedb5440584
Alexis Es Soureifi <alexisfmpro2007@yahoo.com>
2022-09-13 19:42:54 +0300

    add global authorization in filterConfig

--- GroupProject/App_Start/FilterConfig.cs
+++ GroupProject/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace GroupProject
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new AuthorizeAttribute());
         }
     }
 }
